ca7d19ab5b0ee86afab0609bda5c05dcca51b19f
Jonathan Pacheco <jonathan@lifeblue.com>
2013-04-26 11:46:44 -0500

    Documentation and DUST

--- js/themes/admin/actions.js
+++ js/themes/admin/actions.js
@@ -7,9 +7,56 @@
  * @todo 
  */
 
-(function ($, Admin, undefined) {
+(function ( $, Admin, dust, undefined ) {
 
     "use strict";
+
+    Admin.dust = {
+        compileTemplate: function ( template ) {
+            return dust.compile( template.output, template.name );
+        },
+        loadTemplate: function ( template ) {
+            dust.loadSource( template );
+        },
+        build: function ( template, data ) {
+            var result;
+
+            dust.render( template, data, function dustRender( errors, output ) {
+                result = output;
+            });
+
+            return result;
+        },
+        init: function ( template, data ) {
+            Admin.dust.loadTemplate( Admin.dust.compileTemplate( template ) );
+
+            return Admin.dust.build( template.name, data );
+        }
+    };
+
+    Admin.templates = {
+        testTemplate: {
+            name: "test",
+            output: [ 
+                "<h1>{name}</h1>",
+                "<p>What a dope.</p>"
+            ].join("\n")
+        },
+        secondaryNav: {
+            name: "secondaryNav",
+            output: [
+                "<ul class='unstyled vertical-nav {ulClass}'>",
+                    "{#navigation}",
+                        "<li>",
+                            "<a href='{url}'>",
+                                "{name}",
+                            "</a>",
+                        "</li>",
+                    "{/navigation}",
+                "</ul>"
+            ].join("\n")
+        }
+    };
 
     /**
      * AJAX Form Submission & Response
@@ -28,12 +75,14 @@
 
         config: {
             error: false,
+            provideMessage: true,
             successText: "Great! Your form has been submitted.",
             errorText: "There was an error submitting your form. Please contact the site's administrator.",
             alertClass: "alert panel",
             alertSuccessClass: "success",
             alertErrorClass: "error",
             messageLocation: "append",
+            clearForm: true,
             fieldSuccessClass: "success",
             scrollElem: null,
             callback: null
@@ -44,15 +93,15 @@
          * Grabs config and passes it and form on to `onSubmit`
          */
 
-        init: function (config) {
-            var $this = Admin.ajaxForm,
+        init: function ( config ) {
+            var context = Admin.ajaxForm,
                 thisForm = $(this),
 
                 // If config are not supplied, pass an empty object
                 settings = config || {};
 
             // Call the submission method, passing on this form and settings
-            $this.onSubmit.call(thisForm, settings);
+            context.onSubmit.call( thisForm, settings );
         },
 
         /**
@@ -61,8 +110,8 @@
          * both on success and error
          */
 
-        onSubmit: function (config) {
-            var $this = Admin.ajaxForm,
+        onSubmit: function ( config ) {
+            var context = Admin.ajaxForm,
                 settings = config,
                 thisForm = $(this);
 
@@ -70,24 +119,34 @@
             $.ajax({
                 type: thisForm.attr("method"),
                 url: thisForm.attr("action"),
-                data: decodeURIComponent(thisForm.serialize()),
+                data: decodeURIComponent( thisForm.serialize() ),
                 cache: false,
 
                 // If successful...
-                success: function ajaxSuccess() {
+                success: function ajaxSuccess( data ) {
 
-                    // ...call the after-submission method
-                    $this.afterSubmit.call(thisForm, settings);
+                    if ( settings.provideMessage ) {
+
+                        // ...call the after-submission method
+                        context.afterSubmit.call( thisForm, settings );
+                    }
+
+                    return data;
                 },
 
                 // If unsuccessful...
-                error: function ajaxError() {
+                error: function ajaxError( data ) {
 
-                    // ...set `error` to be true...
-                    settings.error = true;
+                    if ( settings.provideMessage ) {
 
-                    // ...then call the after-submission method
-                    $this.afterSubmit.call(thisForm, settings);
+                        // ...set `error` to be true...
+                        settings.error = true;
+
+                        // ...then call the after-submission method
+                        context.afterSubmit.call( thisForm, settings );
+                    }
+
+                    return data;
                 }
             });
         },
@@ -98,29 +157,29 @@
          * and place message, runs callback (if there)
          */
 
-        afterSubmit: function (config) {
-            var $this = Admin.ajaxForm,
+        afterSubmit: function ( config ) {
+            var context = Admin.ajaxForm,
                 thisForm = $(this),
-                settings = $.extend($this.config, config),
+                settings = $.extend( context.config, config ),
 
                 // Call the method to build the response message
-                responseMessage = $this.buildMessage(settings);
+                responseMessage = context.buildMessage( settings );
 
             // If there was no error...
-            if (!settings.error) {
+            if ( !settings.error ) {
 
                 // ...call method to reset form
-                $this.resetForm.call(thisForm, settings);
+                context.resetForm.call( thisForm, settings );
             }
 
             // Call method to place response message
-            $this.placeMessage.call(thisForm, settings, responseMessage);
+            context.placeMessage.call( thisForm, settings, responseMessage );
 
             // Then, if a callback function has been supplied...
-            if (typeof settings.callback === "function") {
+            if ( typeof settings.callback === "function" ) {
 
                 // ...call it, passing on this form as context
-                settings.callback.call(thisForm);
+                settings.callback.call( thisForm );
             }
         },
 
@@ -130,7 +189,7 @@
          * Returns string
          */
 
-        buildMessage: function (config) {
+        buildMessage: function ( config ) {
             var message = '<div class="';
 
             // Build out the message using our options
@@ -151,16 +210,19 @@
          * class from fields
          */
 
-        resetForm: function (config) {
+        resetForm: function ( config ) {
             var thisForm = $(this);
 
-            // Reset this form
-            thisForm[0].reset();
+            if ( config.clearForm ) {
 
-            // Remove "success" classes from form fields
-            thisForm
-                .find('label')
-                .removeClass(config.fieldSuccessClass);
+                // Reset this form
+                thisForm[0].reset();
+
+                // Remove "success" classes from form fields
+                thisForm
+                    .find("label")
+                    .removeClass( config.fieldSuccessClass );
+            }
         },
 
         /**
@@ -169,37 +231,37 @@
          * places message
          */
 
-        placeMessage: function (config, message) {
-            var $this = Admin.ajaxForm,
+        placeMessage: function ( config, message ) {
+            var context = Admin.ajaxForm,
                 thisForm = $(this),
                 responseMessage = $(message);
 
             // If options tell us to prepend the message...
-            if (config.messageLocation === "prepend") {
+            if ( config.messageLocation === "prepend" ) {
 
                 // ...place it at the top of the form
-                thisForm.prepend(responseMessage);
+                thisForm.prepend( responseMessage );
 
             // If options tell us to append the message...
-            } else if (config.messageLocation === "append") {
+            } else if ( config.messageLocation === "append" ) {
 
                 // ...place it at the bottom of the form
-                thisForm.append(responseMessage);
+                thisForm.append( responseMessage );
 
             // If options tell us to place the message before...
-            } else if (config.messageLocation === "before") {
+            } else if ( config.messageLocation === "before" ) {
 
                 // ...place it before the form
-                thisForm.before(responseMessage);
+                thisForm.before( responseMessage );
 
             // If options tell us to place the message after...
-            } else if (config.messageLocation === "after") {
+            } else if ( config.messageLocation === "after" ) {
 
                 // ...place it after the form
-                thisForm.after(responseMessage);
+                thisForm.after( responseMessage );
             }
 
-            $this.goToMessage(config, responseMessage);
+            context.goToMessage( config, responseMessage );
         },
 
         /**
@@ -207,16 +269,16 @@
          * Right now, crudely animates to placed message
          */
 
-        goToMessage: function (config, message) {
-            var $this = Admin.ajaxForm,
+        goToMessage: function ( config, message ) {
+            var context = Admin.ajaxForm,
 
                 // If element isn't specified, scroll `html`/`body`
                 scrollElem = config.scrollElem || $("html, body");
 
             scrollElem.animate({
                 scrollTop: message.offset().top
-            }, 500);
-            $this.removeMessage(message);
+            }, 500 );
+            context.removeMessage( message );
         },
 
         /**
@@ -225,11 +287,11 @@
          * after timeout
          */
 
-        removeMessage: function (message) {
+        removeMessage: function ( message ) {
 
             // After a few seconds, remove message
             setTimeout(function () {
-                message.slideUp(150);
+                message.slideUp( 150 );
             }, 8000, function () {
                 message.remove();
             });
@@ -245,7 +307,7 @@
      * @todo 
      */
 
-     Admin.verticalNav = {
+    Admin.verticalNav = {
 
         /**
          * Config
@@ -255,6 +317,7 @@
             navWrapper: $("#vertical-nav-wrapper"),
             hideClass: "off-left",
             revealClass: "off-right",
+            template: Admin.templates.secondaryNav,
             url: "/slice/admin/sample-nav.php"
         },
 
@@ -262,82 +325,75 @@
           * Init
           */
 
-        init: function (config) {
-            var $this = Admin.verticalNav,
-                settings = $.extend(true, {}, $this.config, config || {});
+        init: function ( config ) {
+            var context = Admin.verticalNav,
+                settings = $.extend( true, {}, context.config, config || {} );
 
             // Call the nav hiding method
-            $this.hideNav(settings);
+            context.hideNav( settings );
         },
 
-        hideNav: function (config) {
-            var $this = Admin.verticalNav;
+        hideNav: function ( config ) {
+            var context = Admin.verticalNav;
 
             // Find the wrapper's child, hide it with class
-            config.navWrapper.children().addClass(config.hideClass);
+            config.navWrapper.children().addClass( config.hideClass );
 
             // Call the method to remove the old nav
-            $this.removeNav(config);
+            context.removeNav( config );
         },
 
-        removeNav: function (config) {
-            var $this = Admin.verticalNav;
+        removeNav: function ( config ) {
+            var context = Admin.verticalNav;
 
             // CRUDE: Wait for CSS Animation
-            setTimeout(function () {
+            setTimeout( function () {
 
                 // Clear out the html
                 config.navWrapper.html("");
 
                 // Call method to get new nav
-                $this.getNav(config);
-            }, 250);
+                context.getNav( config );
+            }, 250 );
         },
 
-        getNav: function (config) {
-            var $this = Admin.verticalNav;
+        getNav: function ( config ) {
+            var context = Admin.verticalNav;
 
             // Via AJAX, get JSON string of new nav
-            $.getJSON(config.url, function getNavJSON (data) {
-                $this.buildNav(data, config);
+            $.getJSON( config.url, function getNavJSON( data ) {
+                context.buildNav( data, config );
             });
         },
 
-        buildNav: function (data, config) {
-            var $this = Admin.verticalNav,
-                items = "<ul class='unstyled vertical-nav " + config.revealClass + "'>";
+        buildNav: function ( data, config ) {
+            var context = Admin.verticalNav,
+                items;
 
-            // Loop through each object, build new nav
-            $.each(data.navigation, function navlistItems (i, item) {
-                items += "<li><a href='";
-                items += item.url;
-                items += "'>";
-                items += item.name;
-                items += "</a></li>";
-            });
-            items += "</ul>";
+            data.ulClass = config.revealClass;
+            items = Admin.dust.init( config.template, data );
 
             // Call method to place new nav
-            $this.placeNav(items, config);
+            context.placeNav( items, config );
 
             return items;
         },
 
-        placeNav: function (nav, config) {
-            var $this = Admin.verticalNav,
+        placeNav: function ( nav, config ) {
+            var context = Admin.verticalNav,
                 newNav = $(nav);
 
             // Append new nav in wrapper
-            config.navWrapper.append(newNav);
+            config.navWrapper.append( newNav );
 
             // Call method to show new nav
-            $this.showNav(newNav, config);
+            context.showNav( newNav, config );
         },
 
-        showNav: function (nav, config) {
+        showNav: function ( nav, config ) {
 
             // Show it, yo
-            nav.removeClass(config.revealClass);
+            nav.removeClass( config.revealClass );
         }
     };
 
@@ -351,18 +407,21 @@
      */
 
     Admin.init = function () {
-        $("form").on("submit", function (event) {
-            Admin.ajaxForm.init.call($(this), {
+        console.log( Admin.dust.init( Admin.templates.testTemplate, { name: "Chad" } ) );
+
+
+        $("form").on( "submit", function ( event ) {
+            Admin.ajaxForm.init.call( $(this), {
                 scrollElem: $(".content-page")
             });
             event.preventDefault();
         });
-        $("#vertical-nav-wrapper").on("click", "a", function (event) {
-            Admin.verticalNav.init.call($(this));
+        $("#vertical-nav-wrapper").on( "click", "a", function ( event ) {
+            Admin.verticalNav.init.call( $(this) );
             event.preventDefault();
         });
-        $("#content-nav-back").on("click", function (event) {
-            Admin.verticalNav.init.call($(this), {
+        $("#content-nav-back").on( "click", function ( event ) {
+            Admin.verticalNav.init.call( $(this), {
                 hideClass: "off-right",
                 revealClass: "off-left"
             });
@@ -372,4 +431,4 @@
 
     Admin.init();
 
-}(jQuery, window.Admin = window.Admin || {}));
+}( jQuery, window.Admin = window.Admin || {}, window.dust ) );
