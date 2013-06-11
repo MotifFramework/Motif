5c608d94436b5b45cdf567b5ff62d6ad82fa35a7
Derek Odell <derek@derekodell.com>
2013-06-10 21:22:19 -0500

    control picker

--- js/admin/tinymce3.4/plugins/lbcontrols/editor_plugin.js
+++ js/admin/tinymce3.4/plugins/lbcontrols/editor_plugin.js
@@ -7,52 +7,52 @@
  */
 
 (function() {
-	tinymce.create('tinymce.plugins.LBControls', {
-		init : function(ed, url) {
-			// Register commands
-			ed.addCommand('mceLBControls', function() {
-				
-				$("<a></a>").lb_box({
-					dataSource : "/admin/content/dialog/controlPicker.html?flushBackground=1&tiny=1",
-					title : "Pick A Control...",
-					width : 600,
-					height : 400,
-					type: "iframe",
-					animate : true,
-					id : "ControlPickerWindow"
-				}).click();
-				
-				/*ed.windowManager.open({
-					file : url + '/lbcontrols.htm',
-					width : 10,
-					height : 10,
-					inline : 1
-				}, {
-					plugin_url : url
-				});*/
-				
-			});
+    tinymce.create('tinymce.plugins.LBControls', {
+        init : function(ed, url) {
+            // Register commands
+            ed.addCommand('mceLBControls', function() {
 
-			// Register buttons
-			ed.addButton('lbcontrols', {
-				title : 'Manage Controls',
-				cmd : 'mceLBControls',
-				image : ''+url+'/img/application_view_tile.png',
-				icon_src : ''+url+'/img/application_view_tile.png'
-			});
-		},
+                $("<a></a>").lb_box({
+                    dataSource : "/admin/dialog/controlPicker.html?flushBackground=1&tiny=1",
+                    title : "Pick A Control...",
+                    width : 600,
+                    height : 400,
+                    type: "iframe",
+                    animate : true,
+                    id : "ControlPickerWindow"
+                }).click();
 
-		getInfo : function() {
-			return {
-				longname : 'LB Control Manager',
-				author : 'LifeBlue',
-				authorurl : 'http://www.lifeblue.com',
-				infourl : 'http://www.lifeblue.com',
-				version : tinymce.majorVersion + "." + tinymce.minorVersion
-			};
-		}
-	});
+                /*ed.windowManager.open({
+                    file : url + '/lbcontrols.htm',
+                    width : 10,
+                    height : 10,
+                    inline : 1
+                }, {
+                    plugin_url : url
+                });*/
 
-	// Register plugin
-	tinymce.PluginManager.add('lbcontrols', tinymce.plugins.LBControls);
+            });
+
+            // Register buttons
+            ed.addButton('lbcontrols', {
+                title : 'Manage Controls',
+                cmd : 'mceLBControls',
+                image : ''+url+'/img/application_view_tile.png',
+                icon_src : ''+url+'/img/application_view_tile.png'
+            });
+        },
+
+        getInfo : function() {
+            return {
+                longname : 'LB Control Manager',
+                author : 'LifeBlue',
+                authorurl : 'http://www.lifeblue.com',
+                infourl : 'http://www.lifeblue.com',
+                version : tinymce.majorVersion + "." + tinymce.minorVersion
+            };
+        }
+    });
+
+    // Register plugin
+    tinymce.PluginManager.add('lbcontrols', tinymce.plugins.LBControls);
 })();
