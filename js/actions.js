9f3934bf5179d89dea981fbf295216ef9eacebbe
Jonathan Pacheco <jonathan@lifeblue.com>
2014-10-02 11:55:43 -0500

    Training material prepped for Herald, Gauntlet

--- js/actions.js
+++ js/actions.js
@@ -7,38 +7,16 @@
     "use strict";
 
     var App = function () {
-        this.initUI();
+        this.initScroll();
         this.initForms();
     };
 
-    App.prototype.initUI = function () {
-        $(".js-tabs").plugin("tabs", {
-            "cssTransition": Modernizr.csstransitions
-        });
-        $(".canvas-trigger").plugin("reveal", {
-            "type": "exclusive",
-            "activeClass": "is-active",
-            "visitedClass": "was-active"
-        });
-        $(".js-reveal").plugin("reveal", {
-            "type": "exclusive",
-            "activeClass": "is-revealed",
-            "visitedClass": "was-revealed"
-        });
-        $(".js-fade").plugin("reveal", {
-            "activeClass": "is-faded",
-            "visitedClass": "was-faded"
-        });
-        $(".js-expand").plugin("reveal", {
-            "type": "exclusive",
-            "activeClass": "is-expanded",
-            "visitedClass": "was-expanded"
-        });
+    App.prototype.initScroll = function () {
+        
     };
 
     App.prototype.initForms = function () {
-        $("[data-validation='ajax']").plugin("ajaxSubmission");
-        $("[data-validation='true']").plugin("gauntlet");
+        
     };
 
     new App();
