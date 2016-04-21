df94ca12c6311e7290a9b19de982bfb5cd92d673
johnfischelli <john.fischelli@gmail.com>
2016-04-21 17:20:24 -0500

    get gulp building less files

---
+++ gulp/tasks/less-fixed-build.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+ 
+gulp.task('less-fixed', function() {
+    gulp.src('./less/global-fixed.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        // .pipe(cleanCSS({debug: true}, function(details) {
+        //     console.log(details.name + ': ' + details.stats.originalSize);
+        //     console.log(details.name + ': ' + details.stats.minifiedSize);
+        // }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./gulpbuild/css'));
+});
