aae6cc074e1613e6fccfe0c06b3a7e4967f5394a
Jonathan Pacheco <jonathan@lifeblue.com>
2016-09-19 15:33:46 -0500

    Updating shrinkwrap and version number for Sprite Icon

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,58 @@
-module.exports = function ( grunt ) {
+module.exports = function (grunt) {
 
-    // Load the plugins that provide the tasks.
-    require("load-grunt-config")(grunt);
+  // Load the plugins that provide the tasks.
+  require('load-grunt-config')(grunt)
 
-    // Default task, compiles LESS and JS into "build" folder
-    grunt.registerTask("default", [
-        "less:globalBuild",
-        "less:globalFixedBuild",
-        "uglify:build"
-    ]);
+  // Default task, compiles LESS and JS into "build" folder
+  grunt.registerTask('default', [
+    'less:globalBuild',
+    'less:globalFixedBuild',
+    'uglify:build'
+  ])
 
-    // Run when you want to refresh everything
-    grunt.registerTask("refresh", [
-        "svgstore:build",
-        "less:globalBuild",
-        "less:globalFixedBuild",
-        "uglify:build"
-    ]);
-    grunt.registerTask("build", [
-        "svgstore:build",
-        "less:globalBuild",
-        "less:globalFixedBuild",
-        "uglify:build"
-    ]);
+  // Run when you want to refresh everything
+  grunt.registerTask('refresh', [
+    'svgstore:build',
+    'less:globalBuild',
+    'less:globalFixedBuild',
+    'uglify:build'
+  ])
+  grunt.registerTask('build', [
+    'svgstore:build',
+    'less:globalBuild',
+    'less:globalFixedBuild',
+    'uglify:build'
+  ])
 
-    // Distribution Build
-    grunt.registerTask("dist", [
-        "svgmin:dist",
-        "svgstore:dist",
-        "less:globalDist",
-        "less:globalFixedDist",
-        "uglify:dist"
-    ]);
+  // Distribution Build
+  grunt.registerTask('dist', [
+    'svgmin:dist',
+    'svgstore:dist',
+    'less:globalDist',
+    'less:globalFixedDist',
+    'uglify:dist'
+  ])
 
-    // Compile Dev Webfonts
-    grunt.registerTask("fonts", [
-        "svgstore:build"
-    ]);
+  // Compile Dev Icons
+  grunt.registerTask('icons', [
+    'svgstore:build'
+  ])
+  grunt.registerTask('icons-build', [
+    'svgstore:build'
+  ])
+  grunt.registerTask('icons-dist', [
+    'svgmin:dist',
+    'svgstore:dist'
+  ])
 
-    // Compile Dev LESS Files
-    grunt.registerTask("less-build", [
-        "less:globalBuild",
-        "less:globalFixedBuild"
-    ]);
+  // Compile Dev LESS Files
+  grunt.registerTask('less-build', [
+    'less:globalBuild',
+    'less:globalFixedBuild'
+  ])
 
-    // Compile Dev JS
-    grunt.registerTask("js", [
-        "uglify:build"
-    ]);
-
-};
+  // Compile Dev JS
+  grunt.registerTask('js', [
+    'uglify:build'
+  ])
+}
