4a09b637ef610395719363a05a2ad93e6d4cd297
Jonathan Pacheco <jonathan@lifeblue.com>
2018-10-08 14:38:28 -0500

    Added Dynamic Responsive Images script, updated Actions

---
+++ js/images/motif.dynamic-responsive-images.js
@@ -0,0 +1,28 @@
+import "lazysizes";
+import getAnimationFrame from './../utils/motif.animationFrame.es6.js';
+
+const LAZYLOAD_CLASS = 'lazyload';
+const animationFrame = getAnimationFrame();
+
+export default function (IMAGE_CLASS = 'js-dynamic-image', multiple = 1) {
+  bindImages(IMAGE_CLASS, multiple);
+}
+
+function bindImages (IMAGE_CLASS, multiple) {
+  document.addEventListener('lazybeforesizes', function (ev) {
+    if (ev.defaultPrevented || !ev.target.classList.contains(IMAGE_CLASS)) {
+      return;
+    }
+
+    const element = ev.target;
+    const url = `${element.getAttribute('data-original')}&w=${element.parentElement.offsetWidth * multiple}&h=${element.parentElement.offsetHeight * multiple}`
+    
+    animationFrame(() => {
+      element.setAttribute('data-src', url);
+
+      if (element.classList.contains(LAZYLOAD_CLASS)) {
+        element.setAttribute('src', url);
+      }
+    });
+  });
+}
