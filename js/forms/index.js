5d3d4263e0b8b6a45e6c610d9c5837801d0524e2
Travis Self <travis@lifeblue.com>
2020-07-08 15:37:20 -0500

    Adding loading button baseline files.

--- js/forms/index.js
+++ js/forms/index.js
@@ -2,25 +2,33 @@ import $ from 'jquery'
 import Gauntlet from 'motif-gauntlet'
 import AjaxSubmission from 'motif-ajaxsubmission'
 import SyncInput from './motif.sync-input'
+import LoadingButton from './motif.loadingButton'
 
 export default function () {
   initAjaxSubmission()
   initValidation()
   initSyncInput()
+  initLoadingButtons()
 }
 
-function initAjaxSubmission () {
+function initAjaxSubmission() {
   $("[data-validation='ajax']").ajaxSubmission()
 }
 
-function initValidation () {
+function initValidation() {
   $("[data-validation='true']").gauntlet()
 }
 
-function initSyncInput () {
+function initSyncInput() {
   ;[].forEach.call(document.querySelectorAll('.js-sync-input'), el => {
     return new SyncInput(el, {
       dispatchChange: true
     })
+  })
+}
+
+function initLoadingButtons() {
+  ;[].forEach.call(document.querySelectorAll('.js-loading-button'), el => {
+    return new LoadingButton(el, {})
   })
 }
---
+++ js/forms/motif.loadingButton.js
@@ -0,0 +1,88 @@
+/*!
+ * @author Travis Self
+ * @author Jonathan Pacheco
+ */
+
+import { uHasClass, uAddClass, uRemoveClass } from '../utils/motif.utilities'
+
+export default class LoadingButton {
+    constructor(element, options) {
+        this.element = element
+        const defaults = {
+            loadingClass: 'is-loading',
+            loadingText: 'Loading...',
+            trigger: 'submit',
+            event: ''
+        }
+        this.settings = { ...defaults, ...options }
+
+        if (this.element) {
+            this.init()
+        }
+    }
+
+    init() {
+        console.log(this.settings)
+        this.prepButton()
+        this.bindEvents()
+    }
+
+    prepButton() {
+        this.element.innerHTML = `
+      <span class="loadingButton__reset">${this.element.innerHTML}</span>
+      <span class="loadingButton__loader">
+        <span class="loadingButton__text">
+            ${this.settings.loadingText}
+        </span>
+      </span>
+    `
+    }
+
+    bindEvents() {
+        if (this.settings.trigger == 'click') {
+            this.element.addEventListener('click', ev => {
+                this.initLoading()
+            })
+        } else if (this.settings.trigger == 'event') {
+            console.log("event")
+            document.addEventListener(this.settings.event, ev => {
+                console.log("event triggered")
+                this.initLoading()
+            })
+        } else {
+            this.element.form.addEventListener('submit', ev => {
+                this.initLoading()
+            })
+        }
+    }
+
+    initLoading() {
+        this.disableButton()
+        this.addLoadingStyles()
+    }
+
+    disableButton() {
+        this.element.disabled = true
+    }
+
+    enableButton() {
+        this.element.disabled = false
+    }
+
+    addLoadingStyles() {
+        if (!uHasClass(this.element, this.settings.loadingClass)) {
+            uAddClass(this.element, this.settings.loadingClass)
+        }
+    }
+
+    removeLoadingStyles() {
+        if (uHasClass(this.element, this.settings.loadingClass)) {
+            uRemoveClass(this.element, this.settings.loadingClass)
+        }
+    }
+
+    reset() {
+        this.enableButton()
+        this.removeLoadingStyles()
+    }
+}
